c0d93f6eb5e8eb5a8d09bac6f692161c279feb4e
unknown <Lera@.(none)>
2011-08-09 20:41:35 +0300

    Added comment to 01_01.c

--- 01_01.c
+++ 01_01.c
@@ -1,3 +1,7 @@
+/*
+Exercise 1-1. Run the ``hello, world'' program on your system. Experiment with leaving out parts of the program, to see what error messages you get.
+*/
+
 #include <stdio.h>
 
 main() {
