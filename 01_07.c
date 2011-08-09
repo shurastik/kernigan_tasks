2d7ae21d03de4fd1e5f4254f5383934fe4b7b0cb
Alexander Pirogov <shurastik@inbox.lv>
2011-08-09 14:35:53 +0300

    task 7

---
+++ 01_07.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int current_char;
+	int space_count = 0;
+	while ((current_char = getchar()) != EOF) {
+		if (current_char == ' ') {
+			if (space_count == 0) {
+				space_count++;
+				printf("%c", current_char);
+			}
+		} else {
+			space_count = 0;
+			printf("%c", current_char);
+		}
+	}
+}
