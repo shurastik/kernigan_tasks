f5e61d612a4104aa8c6a5893d769cce5cd7c0d7a
Alexander Pirogov <shurastik@inbox.lv>
2011-08-09 10:52:48 +0300

    Added first 5 tasks

---
+++ 01_02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	int c;
+    for (c = 0; c < 255; c++) {
+        printf("%c ", c);
+    }
+}
---
+++ 01_03.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int lower, upper, step;
+    float fahr, celsius;
+    lower = 0;
+    upper = 300;
+    step = 20;
+    fahr = lower;
+    printf("\n\nFAHR   CELS\n");
+    printf("---- ------\n");
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%4.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
