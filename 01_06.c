b11aa630ab72982552fc6944f59e63cbafbf1eb6
Alexander Pirogov <shurastik@inbox.lv>
2011-08-09 12:23:05 +0300

    Task 1.6

---
+++ 01_06.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	long nc;
+	int c;
+	nc = 0;
+	while ((c = getchar()) != EOF) {
+		if (c == ' ' || c == '\t' || c == '\n') {
+			++nc;
+			printf("%1ld\n", nc);
+		}
+	}
+}
