18784d81c75d32571a93aba12ea49740ba29286b
Alexander Pirogov <shurastik@inbox.lv>
2011-08-11 16:55:23 +0300

    added loop

--- 01_12.c
+++ 01_12.c
@@ -8,5 +8,9 @@ Exercise 1-12. Write a program that prints its input one word per line.
 #define FALSE	0
 
 main() {
-	
+	int ch;
+	int in_word = FALSE;
+	while ((ch = getchar()) != EOF) {
+		
+	}
 }
